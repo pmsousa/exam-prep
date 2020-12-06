3bff4ca3fab180dc481907db10ded9345d74f44e
psousa75 <pmsousa@gmail.com>
2020-12-06 17:47:41 +0000

    Az-104

--- AZ-104/Subscription Management/Tags.ps1
+++
@@ -1,9 +0,0 @@
-Login-AzAccount
-
-$subscription = Get-AzSubscription | Out-GridView -OutputMode Single -Title 'Select subscription'
-
-Set-AzContext -Subscription $subscription
-
-
-Get-AzTag -Detailed
-
